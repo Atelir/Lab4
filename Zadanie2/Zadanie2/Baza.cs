e516fed57d9759756962e9be25b15b1431a078d8
Atelir <atelir911@gmail.com>
2021-03-16 19:51:09 +0100

    Zadanie

---
+++ Zadanie2/Zadanie2/Baza.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Zadanie2
+{
+    public class Baza
+    {
+        private IDbConnection _connection;
+        public Baza(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+        
+        public IEnumerable<Customers> GetCustomers() //<Customers> zeby nie bylo dynamic
+        {
+
+            return _connection.Query<Customers>("SELECT * FROM dbo.Customers");
+
+        }
+
+        public Customers GetCustomersByID(string id) //parametr
+        {
+
+            return _connection.QuerySingleOrDefault<Customers>("SELECT * FROM dbo.Customers WHERE CustomerID = @ID", new {ID = id });
+
+        }
+
+        public bool AddCustomer(Customers customer)
+        {
+
+            var wynik = _connection.Execute("INSERT INTO Customers(CustomerID,CompanyName) VALUES (@ID, @CompanyName)", new { id = customer.CustomerID, CompanyName = customer.CompanyName });
+            return wynik == 1;
+
+        }
+
+        public Customers ChangeCustomer(string id, string companyName)
+        {
+
+            return _connection.QuerySingleOrDefault<Customers>("UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @ID", new { ID = id, CompanyName = companyName });
+
+        }
+
+        public Customers DeleteCustomer(string id)
+        {
+
+            return _connection.QuerySingleOrDefault<Customers>("DELETE Customers WHERE CustomerID = @ID", new { ID = id });
+
+        }
+
+
+    }
+}
---
+++ Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            var db = new Baza(@"Data Source = ELO420\SQLEXPRESS; Initial Catalog = Northwind; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+            foreach (Customers customers in db.GetCustomers())
+            {
+                Console.WriteLine($"{customers.CustomerID}  {customers.CompanyName}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pojedynczy rekord po ID:");
+            Console.WriteLine();
+            var singleCustomer = db.GetCustomersByID("ALFKI");
+            Console.WriteLine($"{singleCustomer.CustomerID}  {singleCustomer.CompanyName}");
+
+            Console.WriteLine("Podaj nowe ID klienta:");
+            string clientID = Console.ReadLine();
+            Console.WriteLine("Podaj nową nazwę firmy:");
+            string companyName = Console.ReadLine();
+            db.AddCustomer(new Customers() { CustomerID = clientID, CompanyName = companyName });
+            var dlatestu = db.GetCustomersByID(clientID);
+            Console.WriteLine("Wiersz wstawiony:");
+            Console.WriteLine($"{dlatestu.CustomerID}  {dlatestu.CompanyName}");
+            Console.WriteLine("Podaj poprawiona nazwę firmy:");
+            string companyNameEdit = Console.ReadLine();
+            db.ChangeCustomer(clientID, companyNameEdit);
+            db.DeleteCustomer(clientID);
+            foreach (Customers customers in db.GetCustomers())
+            {
+                Console.WriteLine($"{customers.CustomerID}  {customers.CompanyName}");
+            }
+
+
+        }
+
+    }
+}
