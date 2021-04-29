be89cefa93ae13203d7e1513961d6bdad011b97e
Atelir <atelir911@gmail.com>
2021-04-29 17:59:50 +0200

    Zadanie3

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Zadanie3.Scaffold;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using var db = new NorthwindContext();
+
+            db.Add(new Users() { });
+
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
